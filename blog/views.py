41e147ec5117d57e10c1f18ae79bcfbb96f20dab
Bhavesh <bhaveshdaswani93@gmail.com>
2025-01-31 13:56:04 +0530

    initial commit

---
+++ blog/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def index(request):
+  pass
+
+def posts(request):
+  pass
+
+def post_detail(request, slug):
+  print(slug)
+  pass
