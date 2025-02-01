414a62273dfc385911dc5c29d5733dfa4cd1aa71
Bhavesh Daswani <bhaveshdaswani93@gmail.com>
2025-02-01 14:04:03 +0530

    Implement blog structure with templates, views, and static files

--- blog/urls.py
+++ blog/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-  path('/', views.index, name='starting-page'),
-  path('/posts', views.posts, name='post-list'),
-  path('/posts/<slug:slug>', views.post_detail, name='post-detail')
+  path('', views.index, name='starting-page'),
+  path('posts', views.posts, name='post-list'),
+  path('posts/<slug:slug>', views.post_detail, name='post-detail')
 ]
