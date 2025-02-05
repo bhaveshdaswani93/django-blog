e426316dac0763ce68ec29c0643bdeb6a785f035
Bhavesh <bhaveshdaswani93@gmail.com>
2025-02-05 13:38:07 +0530

    working on single post view

--- blog/views.py
+++ blog/views.py
@@ -1,22 +1,70 @@
 from django.shortcuts import render
 from datetime import date
 
-posts = [
-  {
-    'slug': 'hike-in-the-mountains',
-    'image': 'mountains.jpg',
-    'author': 'Yan',
-    'date': date(2021, 8, 12),
-    'title': 'Mountain Hiking',
-    'excerpt': 'There\'s nothing like the views you get when hiking in the mountains! And I wasn\'t even prepared for what happened on this hike.',
-    'content': """
-          lorem ipsum dolor sit amet, consectetur adipiscing elit.
-          Sed ut ultricies risus. Nullam eget libero at nunc
-          maximus dictum. Nullam porttitor, eros nec tristique
-          fermentum, ligula libero suscipit magna, nec pulvinar
-        """
+posts_list = [
+    {
+        "slug": "hike-in-the-mountains",
+        "image": "mountains.jpg",
+        "author": "Maximilian",
+        "date": date(2021, 7, 21),
+        "title": "Mountain Hiking",
+        "excerpt": "There's nothing like the views you get when hiking in the mountains! And I wasn't even prepared for what happened whilst I was enjoying the view!",
+        "content": """
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
+          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
+          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
 
-  }
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
+          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
+          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
+
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
+          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
+          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
+        """
+    },
+    {
+        "slug": "programming-is-fun",
+        "image": "coding.jpg",
+        "author": "Maximilian",
+        "date": date(2022, 3, 10),
+        "title": "Programming Is Great!",
+        "excerpt": "Did you ever spend hours searching that one error in your code? Yep - that's what happened to me yesterday...",
+        "content": """
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
+          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
+          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
+
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
+          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
+          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
+
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
+          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
+          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
+        """
+    },
+    {
+        "slug": "into-the-woods",
+        "image": "woods.jpg",
+        "author": "Maximilian",
+        "date": date(2020, 8, 5),
+        "title": "Nature At Its Best",
+        "excerpt": "Nature is amazing! The amount of inspiration I get when walking in nature is incredible!",
+        "content": """
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
+          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
+          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
+
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
+          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
+          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
+
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
+          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
+          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
+        """
+    }
 ]
 
 # Create your views here.
@@ -24,15 +72,21 @@ def get_sort_key(post):
   return post['date']
 
 def index(request):
-  latest_posts = sorted(posts, key=get_sort_key, reverse=True)
-  latest_posts = list(posts[:3])
+  print(posts_list)
+  latest_posts = sorted(posts_list, key=get_sort_key, reverse=True)
+  latest_posts = list(latest_posts[:3])
 
   return render(request, 'blog/index.html', {
     'posts': latest_posts
   })
 
 def posts(request):
-  return render(request, 'blog/all-posts.html')
+  return render(request, 'blog/all-posts.html', {
+    'posts': posts_list
+  })
 
 def post_detail(request, slug):
-  return render(request, 'blog/post-detail.html')
+  post = next(post for post in posts_list if post['post'] === slug)
+  return render(request, 'blog/post-detail.html', {
+    'post': post
+  })
