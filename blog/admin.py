3d9df3d49c3ecda33f5e25e5a2c6bc97b836c95e
Bhavesh Daswani <bhaveshdaswani93@gmail.com>
2025-02-18 14:01:36 +0530

    Add Author and Tag models; enhance Post model with ManyToMany relationship and admin configuration

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Author, Tag, Post
 
 # Register your models here.
+
+class PostAdmin(admin.ModelAdmin):
+  list_display = ("title", "date")
+  list_filter = ("author", "tags")
+  prepopulated_fields = {"slug": ("title",)}
+
+admin.site.register(Author)
+admin.site.register(Tag)
+admin.site.register(Post, PostAdmin)
--- blog/models.py
+++ blog/models.py
@@ -2,17 +2,34 @@ from django.db import models
 from django.core.validators import MinLengthValidator
 
 class Author(models.Model):
-  first_name: models.CharField(max_length=100)
-  last_name: models.CharField(max_length=100)
-  email: models.EmailField()
+  first_name = models.CharField(max_length=100)
+  last_name = models.CharField(max_length=100)
+  email = models.EmailField()
+
+  def full_name(self):
+    return f'{self.first_name} {self.last_name}'
+  
+  def __str__(self):
+    return self.full_name()
+
+
+class Tag(models.Model):
+  caption = models.CharField(max_length=20)
+
+  def __str__(self):
+    return self.caption
 
 # Create your models here.
 class Post(models.Model):
-  title: models.CharField(max_length=150)
-  excerpt: models.CharField(max_length=200)
-  image: models.CharField(max_length=200)
-  date: models.DateField(auto_now=True)
-  content: models.TextField(validators=[MinLengthValidator(100)])
-  slug: models.SlugField(unique=True, db_index=True)
-  author: models.ForeignKey(Author, null=True, on_delete=models.SET_NULL, related_name='books')
+  title = models.CharField(max_length=150)
+  excerpt = models.CharField(max_length=200)
+  image = models.CharField(max_length=200)
+  date = models.DateField(auto_now=True)
+  content = models.TextField(validators=[MinLengthValidator(100)])
+  slug = models.SlugField(unique=True, db_index=True)
+  author = models.ForeignKey(Author, null=True, on_delete=models.SET_NULL, related_name='books')
+  tags = models.ManyToManyField(Tag, blank=True, related_name='posts')
+
+
+
   
