c28ba37e62cf7b9e1385b4cab8275cadaabc7a19
Bhavesh Daswani <bhaveshdaswani93@gmail.com>
2025-03-08 14:36:47 +0530

    Update DEBUG setting for environment configuration and add Elastic Beanstalk config

--- my_site/settings.py
+++ my_site/settings.py
@@ -24,9 +24,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-b88436aby)82_@17-0b(xt+g*x%ps$2!#5mv1b)260w%3+7_)+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = getenv('IS_DEVELOPMENT', 'True') == 'True'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+  getenv('ALLOWED_HOSTS', 'localhost'),
+]
 
 
 # Application definition
