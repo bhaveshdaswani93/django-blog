efa5a18e0ebc202e380115410c77384429356e4a
Bhavesh Daswani <bhaveshdaswani93@gmail.com>
2025-03-11 14:03:34 +0530

    Update requirements and settings for production readiness

--- my_site/settings.py
+++ my_site/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-b88436aby)82_@17-0b(xt+g*x%ps$2!#5mv1b)260w%3+7_)+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = getenv('IS_DEVELOPMENT', 'True') == 'True'
+DEBUG = False
 
 ALLOWED_HOSTS = [
   getenv('ALLOWED_HOSTS', 'localhost'),
@@ -78,19 +78,19 @@ WSGI_APPLICATION = 'my_site.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
-"""
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-"""
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'postgres',
-        'USER': '',
+        'USER': 'postgres',
         'PASSWORD': 'StrongPassword12345',
         'HOST': 'djangoblogdb.czvrhrbuiwec.us-east-1.rds.amazonaws.com',
         'PORT': '5432'
@@ -134,7 +134,7 @@ USE_TZ = True
 
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
   BASE_DIR / 'static',
